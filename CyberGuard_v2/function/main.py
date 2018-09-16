2036f8826da5069c76d08707c3cb1efe54b02ae0
powered by coffee <31550401+CyberTriber@users.noreply.github.com>
2018-09-16 08:28:11 +0200

    repair of setLevel function
    repair of setLevel function, remove old function

--- CyberGuard_v2/function/main.py
+++ CyberGuard_v2/function/main.py
@@ -59,86 +59,13 @@ def is_in_db(user_id):
         else:
             return False
 
-def poll_exist(user_id):
-    error = False
-    try:
-        getID = db.child('poll').child(user_id).child('created').get().val()
-    except:
-        error = True
-        print("Unexpected error:", sys.exc_info()[0])
-
-    if not error:
-        if getID is not None:
-            return True
-        else:
-            return False
-
-
-def create_poll(user_id, data):
-    error = False
-    print(str(data))
-   
-    print(getTime()+'\t [DEBUG] - '+'create_poll funct:')
-    try:
-        db.child('poll').child(user_id).set(data)
-    except:
-        error = True
-        print("Unexpected error:", sys.exc_info()[0])
-
-def read_poll(user_id):
-    error = False
-    pollData={}
-    try:
-        question = db.child('poll').child(user_id).child('question').get().val()
-        answers = db.child('poll').child(user_id).child('answers').get().val()
-        created = db.child('poll').child(user_id).child('created').get().val()
-        time = db.child('poll').child(user_id).child('time').get().val()
-        results = db.child('poll').child(user_id).child('results').get().val()
-        expire = db.child('poll').child(user_id).child('expire').get().val()
-    except:
-        error = True
-        print("Unexpected error:", sys.exc_info()[0])        
-
-    if not error:
-        pollData = {'question': question, 'answers': answers, 'created': created, 'time': time, 'results': results, 'expire': expire}
-        print(getTime()+'\t [DEBUG] - '+str(pollData))
-    else:
-        pollData = {}
-    return pollData
 
 def setLevel(user_id, newPoints):
     newLevel = 0
-    if newPoints >= 10:
-        newLevel = 2
-        newMultiplier = 2
-
-    if newPoints >= 100:
-        newLevel = 3
-        newMultiplier = 3
-
-    if newPoints >= 500:
-        newLevel = 4
-        newMultiplier = 4
-
-    if newPoints >= 1000:
-        newLevel = 5
-        newMultiplier = 5
-
-    if newPoints >= 2000:
-        newLevel = 6
-        newMultiplier = 6
-
-    if newPoints >= 5000:
-        newLevel = 7
-        newMultiplier = 7
+    for i in range(1,len(LEVEL_SYSTEM)+1):
+	    if newPoints >= LEVEL_SYSTEM.get(i).get('cap'):
+	        newLevel = LEVEL_SYSTEM.get(i).get('level')
+	        newMultiplier = LEVEL_SYSTEM.get(i).get('multiplier')
 
     db.child("users").child(user_id).update({'level': newLevel})
     db.child("users").child(user_id).update({'multiplier': newMultiplier})
-
-	# Level 1 - 0..10 points (multiplier: 1)
-	# Level 2 - 10..100 points (multiplier: 2)
-	# Level 3 - 100..500 points (multiplier: 3)
-	# Level 4 - 500..1000 points (multiplier: 4)
-	# Level 5 - 1000..2000 points (multiplier: 5)
-	# Level 6 - 2000..5000 points (multiplier: 6)
-	# Level 7 - 5000+ points (multiplier: 7)
