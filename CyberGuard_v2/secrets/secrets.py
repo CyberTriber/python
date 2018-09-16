c141db00cb29facbaaea2cbdccdc31ead017dc91
powered by coffee <31550401+CyberTriber@users.noreply.github.com>
2018-09-16 08:11:24 +0200

    Add files via upload

---
+++ CyberGuard_v2/secrets/secrets.py
@@ -0,0 +1,17 @@
+# coding=utf-8
+# Firebase secrets
+CONFIG = {
+  "apiKey": <firebase_api_key>,
+  "authDomain": "<project_name>.firebaseapp.com",
+  "databaseURL": "https://<project_name>.firebaseio.com",
+  "storageBucket": "<project_name>.appspot.com",
+  "serviceAccount": "./secrets/firebase.json"
+}
+
+FBEMAIL = '<name>@discord.app'
+FBPASSWORD = <password>
+
+
+# Discord secrets
+BOT_TOKEN = <botTOKEN>
+OWNER_ID = <bot_owner_discord_id>
